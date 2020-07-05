76870012c8143ee1ceef82e9f60d7ef8aa9de2bb
Lucas Corrêa <lucasrodriguescorrea@gmail.com>
2020-07-04 23:05:32 -0300

    fazendo fitness

---
+++ src/app/app.component.ts
@@ -0,0 +1,427 @@
+import { Component } from '@angular/core';
+import { MazeService } from './maze.service';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'maze-ga';
+
+    public maze: Array<Block> = [];
+    public eltismo: boolean = true;
+    public crossover: number = 0.7;
+    public mutation: number = 0.3;
+    public populationLength: number = 200;
+    public maxGenerations: number = 100;
+    public solution: string = "0001010100000000";
+    public characters: Array<string> = ["00", "01", "10", "11"];
+    public genesTotal: number;
+
+    constructor(private mazeService: MazeService) {
+        this.genesTotal = this.solution.length / 2;
+        this.maze = mazeService.generateMaze();
+    }
+
+    public ngOnInit(): void {
+        this.start();
+    }
+
+    public start(): void {
+        let hasSolution = false;
+        let generation = 0;
+
+        //cria a primeira população aleatória
+        let population = this.createPopulation(this.genesTotal, this.populationLength);
+
+        //verifica se tem a solucao
+        hasSolution = this.verifyHasSolution(population);
+
+        while (!hasSolution && generation < this.maxGenerations) {
+            generation++;
+
+            //cria nova populacao
+            population = this.newGeneration(population, this.eltismo);
+
+            // console.log('population.individuos', population.individuos);
+
+            console.log("Geração " + generation + " | Aptidão: " + population.individuos[0].fitness + " | Melhor: " + population.individuos[0].genes);
+
+            //verifica se tem a solucao
+            hasSolution = this.verifyHasSolutionReal(population.individuos[0]);
+        }
+
+        if (generation == this.maxGenerations) {
+            console.log("Atingiu o número máximo de gerações | " + population.individuos[0].genes + " | Aptidão: " + population.individuos[0].fitness);
+        }
+
+        if (hasSolution) {
+            console.log("Encontrado resultado na geração " + generation + " | " + population.individuos[0].genes + " (Aptidão: " + population.individuos[0].fitness + ")");
+        }
+
+        console.log(population);
+    }
+
+    private verifyHasSolution(population: Population): boolean {
+        let individuo: Individuo = null;
+
+        population.individuos.forEach(i => {
+            if (i.genes == this.solution) {
+                individuo = i;
+                return;
+            }
+        });
+
+        return individuo == null ? false : true;
+    }
+
+    private verifyHasSolutionReal(individuo: Individuo): boolean {
+        let lastField = this.getLastFieldTraveled(individuo);
+        let exitField = this.maze[3];
+
+        return individuo.wallsHit == 0 && !individuo.hadImpossibleMove && exitField.id == lastField.id;
+    }
+
+    private getLastFieldTraveled(individuo: Individuo): Block {
+        let maze = this.maze.slice(0, this.maze.length);
+        maze.forEach(m => m.current = false);
+        maze[15].current = true;
+
+        let currentField = maze.find(m => m.current);
+
+        for (let i = 0; i < individuo.genes.length; i += 2) {
+            let move = individuo.genes[i] + individuo.genes[i + 1];
+
+            currentField = this.getNextMazeFieldByDirection(maze, currentField, move);
+        }
+
+        return currentField;
+    }
+
+    private newGeneration(population: Population, elitismo: boolean): Population {
+        //nova população do mesmo tamanho da antiga
+        let newPopulation = new Population();
+        newPopulation.individuos = [];
+
+        population.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1);
+
+        //se tiver elitismo, mantém o melhor indivíduo da geração atual
+        if (elitismo) {
+            newPopulation.individuos.push(population.individuos[0]);
+        }
+
+        //insere novos indivíduos na nova população, até atingir o tamanho máximo
+        while (newPopulation.individuos.length < this.populationLength) {
+            //seleciona os 2 pais por torneio
+            let parents = this.tournament(population);
+
+            let children = [];
+
+            //verifica a taxa de crossover, se sim realiza o crossover, se não, mantém os pais selecionados para a próxima geração
+            if (this.randomDoubleFromInterval(0, 100) <= this.crossover) {
+                children = this.generateCrossover(parents[1], parents[0]);
+            }
+            else {
+                children.push(parents[0]);
+                children.push(parents[1]);
+            }
+
+            //adiciona os filhos na nova geração
+            newPopulation.individuos.push(children[0]);
+            newPopulation.individuos.push(children[1]);
+        }
+
+        //ordena a nova população
+        newPopulation.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1)
+        return newPopulation;
+    }
+
+    public tournament(population: Population): Array<Individuo> {
+        let newPopulation = new Population();
+        newPopulation.individuos = [];
+
+        //seleciona 3 indivíduos aleatóriamente na população
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+        newPopulation.individuos.push(population.individuos[this.randomIntFromInterval(0, this.populationLength - 1)]);
+
+        //ordena a população
+        newPopulation.individuos.sort((a, b) => (a.fitness < b.fitness) ? 1 : -1);
+
+        let parents: Array<Individuo> = [];
+
+        //seleciona os 2 melhores deste população
+        parents.push(newPopulation.individuos[0]);
+        parents.push(newPopulation.individuos[1]);
+
+        return parents;
+    }
+
+    public generateCrossover(individuo1: Individuo, individuo2: Individuo) {
+        //sorteia o ponto de corte
+        let pontoCorte1 = this.randomIntFromInterval(0, (individuo1.genes.length / 2) - 2) + 1;
+        let pontoCorte2 = this.randomIntFromInterval(0, (individuo1.genes.length / 2) - 2) + individuo1.genes.length / 2;
+
+        let children = [];
+
+        //pega os genes dos pais
+        let parentGene1 = individuo1.genes;
+        let parentGene2 = individuo2.genes;
+
+        //realiza o corte, 
+        let childGene1 = parentGene1.substr(0, pontoCorte1);
+        childGene1 = childGene1 + parentGene2.substr(pontoCorte1, (pontoCorte2 - pontoCorte1));
+        childGene1 = childGene1 + parentGene1.substr(pontoCorte2, (parentGene1.length - pontoCorte2));
+
+        let childGene2 = parentGene2.substr(0, pontoCorte1);
+        childGene2 = childGene2 + parentGene1.substr(pontoCorte1, (pontoCorte2 - pontoCorte1));
+        childGene2 = childGene2 + parentGene2.substr(pontoCorte2, (parentGene2.length - pontoCorte2));
+
+        //cria o novo indivíduo com os genes dos pais
+        children.push(this.createIndividuoByGenes(childGene1));
+        children.push(this.createIndividuoByGenes(childGene2));
+
+        return children;
+    }
+
+    private createPopulation(genesTotal: number, length: number): Population {
+        let population = new Population();
+        population.length = length;
+
+        population.individuos = [];
+        for (let i = 0; i < length; i++) {
+            population.individuos[i] = this.createIndividuo(genesTotal);
+        }
+
+        return population;
+    }
+
+    private createIndividuo(genesTotal: number): Individuo {
+        let individuo = new Individuo();
+        individuo.genes = "";
+        individuo.fitness = 0;
+
+        for (let i = 0; i < genesTotal; i++) {
+            var index = this.randomIntFromInterval(0, 3);
+            individuo.genes = individuo.genes + this.characters[index];
+        }
+
+        //this.generateFitness(individuo);
+        this.generateFitnessReal(individuo);
+
+        return individuo;
+    }
+
+    private createIndividuoByGenes(genes: string): Individuo {
+        let individuo = new Individuo();
+        individuo.genes = genes;
+
+        //se for mutar, cria um gene aleatório
+        if (this.randomDoubleFromInterval(0, 100) <= this.mutation) {
+            let geneNovo = "";
+            let posAleatoria = this.randomIntFromInterval(0, genes.length / 2);
+
+            if (posAleatoria % 2 != 0) {
+                posAleatoria--;
+            }
+
+            if (posAleatoria == 54) {
+                posAleatoria--;
+            }
+
+            for (let i = 0; i < genes.length; i++)
+            {
+                if (i == posAleatoria + 1) continue;
+
+                if (i == posAleatoria) {
+                    geneNovo = geneNovo + this.characters[this.randomIntFromInterval(0, this.characters.length - 1)];
+                }
+                else {
+                    geneNovo = geneNovo + genes[i];
+                }
+
+            }
+            individuo.genes = geneNovo;
+        }
+
+        //this.generateFitness(individuo);
+        this.generateFitnessReal(individuo);
+
+        return individuo;
+    }
+
+    private randomIntFromInterval(min, max): number { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    private randomDoubleFromInterval(min, max): number { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min) / 100;
+    }
+
+    private resetCurrent(): void {
+        this.maze.find(m => {
+            m.current = false;
+        });
+
+        this.maze[15].current = true;
+    }
+
+    private generateFitness(individuo: Individuo): void {
+        if(!individuo.fitness) individuo.fitness = 0;
+
+        for (let i = 0; i < this.solution.length; i += 2) {
+            var path = this.solution[i] + this.solution[i + 1];
+            var genesPath = individuo.genes[i] + individuo.genes[i + 1];
+
+            if (path == genesPath) {
+                individuo.fitness++;
+            }
+        }
+    }
+
+    private generateFitnessReal(individuo: Individuo): void {
+        if (!individuo.fitness) individuo.fitness = 0;
+        if (!individuo.wallsHit) individuo.wallsHit = 0;
+        if (!individuo.repeatedInput) individuo.repeatedInput = 0;
+
+        this.resetCurrent();
+        let fieldsTraveled = [];
+        let currentField = this.maze.find(m => m.current);
+
+        let verifyMove = (direction: Direction, cantMove: boolean): void => {
+            //Verifica se a direção está dentro das possíveis direções do campo atual no labirinto
+            if (currentField.possibleDirections.findIndex(d => d == direction) > -1) {
+
+                console.log('cantMove', cantMove);
+                console.log('cantMove', fieldsTraveled.findIndex(f => f.id == currentField.id));
+
+                //Verifica se a parede na direção que ele deseja ir
+                if (cantMove) {
+                    // console.log('bate na parede');
+                    individuo.wallsHit++;
+                }
+
+                //Verifica o bot já passou por esse campo antes
+                if (fieldsTraveled.findIndex(f => f.id == currentField.id) > -1) {
+                    // console.log('andou mesmo caminho');
+                    individuo.repeatedInput++;
+                }
+                else {
+                    // console.log('anda ok');
+                    fieldsTraveled.push(currentField);
+                }
+            } else {
+                //Verifica o bot já passou por esse campo antes
+                if (fieldsTraveled.findIndex(f => f.id == currentField.id) > -1) {
+                    // console.log('ja passou antes');
+                    individuo.repeatedInput++;
+                }
+                else {
+                    fieldsTraveled.push(currentField);
+                }
+
+
+                individuo.fitness -= 100;
+                individuo.hadImpossibleMove = true;
+                // console.log('individuo.fitness', individuo.fitness);
+            }
+        }
+
+        for (let i = 0; i < individuo.genes.length; i += 2) {
+            let move = individuo.genes[i] + individuo.genes[i + 1];
+
+            switch (move) {
+                case Direction.Left:
+                    verifyMove(move, currentField.wallLeft);
+                    break;
+                case Direction.Top:
+                    verifyMove(move, currentField.wallTop);
+                    break;
+                case Direction.Right:
+                    verifyMove(move, currentField.wallRight);
+                    break;
+                case Direction.Bottom:
+                    verifyMove(move, currentField.wallBottom);
+                    break;
+            }
+
+            currentField = this.getNextMazeFieldByDirection(this.maze, currentField, move);
+        }
+
+        individuo.fitness -= individuo.wallsHit;
+        individuo.fitness -= individuo.repeatedInput;
+
+        this.generatePenalty(individuo, fieldsTraveled);
+    }
+
+    private generatePenalty(individuo: Individuo, fieldsTraveled: Array<Block>): void {
+        let penalty = this.maze[3].cordX - fieldsTraveled[fieldsTraveled.length - 1].cordX;
+        penalty += fieldsTraveled[fieldsTraveled.length - 1].cordY - this.maze[3].cordY;
+        
+        individuo.fitness += penalty;
+    }
+
+    private getNextMazeFieldByDirection(maze: Array<Block>, currentField: Block, move: string): Block {
+        let oldIndex = maze.findIndex(m => m.current);
+
+        if (currentField.possibleDirections.findIndex(d => d == move) > -1) {
+            switch (move) {
+                case Direction.Left:
+                    oldIndex--;
+                    break;
+                case Direction.Top:
+                    oldIndex = oldIndex - 4;
+                    break;
+                case Direction.Right:
+                    oldIndex++;
+                    break;
+                case Direction.Bottom:
+                    oldIndex = oldIndex + 4;
+                    break;
+            }
+
+            this.maze.find(m => {
+                m.current = false;
+            });
+
+            this.maze[oldIndex].current = true;
+        }
+
+        return maze[oldIndex];
+    }
+}
+
+
+export enum Direction {
+    Left = "00",
+    Top = "01",
+    Right = "10",
+    Bottom = "11"
+}
+
+export class Block {
+    public id: number;
+    public cordX: number;
+    public cordY: number;
+    public wallLeft: boolean;
+    public wallRight: boolean;
+    public wallTop: boolean;
+    public wallBottom: boolean;
+    public current: boolean;
+    public active: boolean;
+    public possibleDirections: Array<Direction>;
+}
+
+export class Population {
+    public individuos: Array<Individuo>;
+    public length: number;
+}
+
+export class Individuo {
+    public genes: string;
+    public fitness: number;
+    public wallsHit: number;
+    public repeatedInput: number;
+    public hadImpossibleMove: boolean;
+}
---
+++ src/app/maze.service.ts
@@ -0,0 +1,129 @@
+import { Injectable } from '@angular/core';
+import { Block, Direction } from './app.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MazeService {
+    public maze: Array<Block> = [];
+    constructor() { }
+
+    public generateMaze(): Array<Block> {
+        let cordX = 0;
+        let cordY = 0;
+
+        for (let i = 0; i < 16; i++) {
+            if(cordY == 4){
+                cordY = 0;
+                cordX++; 
+            }
+        
+            this.maze.push(
+                {
+                    id: i,
+                    cordX: cordX,
+                    cordY: cordY,
+                    wallLeft: false,
+                    wallRight: false,
+                    wallTop: false,
+                    wallBottom: false,
+                    active: false,
+                    current: false,
+                    possibleDirections: []
+                }
+            );
+
+            cordY++;
+        }
+
+        //Linha 1
+        //Coluna 1
+        this.maze[0].wallTop = true;
+        this.maze[0].wallLeft = true;
+        this.maze[0].possibleDirections.push(Direction.Right);
+        this.maze[0].possibleDirections.push(Direction.Bottom);
+        //Coluna 2
+        this.maze[1].wallTop = true;
+        this.maze[1].wallBottom = true;
+        this.maze[1].possibleDirections.push(Direction.Left);
+        this.maze[1].possibleDirections.push(Direction.Right);
+        //Coluna 3
+        this.maze[2].wallTop = true;
+        this.maze[2].possibleDirections.push(Direction.Left);
+        this.maze[2].possibleDirections.push(Direction.Right);
+        this.maze[2].possibleDirections.push(Direction.Bottom);
+        //Coluna 4
+        this.maze[3].wallTop = true;
+        this.maze[3].possibleDirections.push(Direction.Left);
+        this.maze[3].possibleDirections.push(Direction.Right);
+        this.maze[3].possibleDirections.push(Direction.Bottom);
+
+        //Linha 2
+        //Coluna 1
+        this.maze[4].wallLeft = true;
+        this.maze[4].possibleDirections.push(Direction.Top);
+        this.maze[4].possibleDirections.push(Direction.Right);
+        this.maze[4].possibleDirections.push(Direction.Bottom);
+        //Coluna 2
+        this.maze[5].wallTop = true;
+        this.maze[5].wallRight = true;
+        this.maze[5].possibleDirections.push(Direction.Left);
+        this.maze[5].possibleDirections.push(Direction.Bottom);
+        //Coluna 3
+        this.maze[6].wallLeft = true;
+        this.maze[6].possibleDirections.push(Direction.Top);
+        this.maze[6].possibleDirections.push(Direction.Right);
+        this.maze[6].possibleDirections.push(Direction.Bottom);
+        //Coluna 4
+        this.maze[7].wallRight = true;
+        this.maze[7].wallBottom = true;
+        this.maze[7].possibleDirections.push(Direction.Left);
+        this.maze[7].possibleDirections.push(Direction.Top);
+
+        //Linha 3
+        //Coluna 1
+        this.maze[8].wallLeft = true;
+        this.maze[8].possibleDirections.push(Direction.Right);
+        this.maze[8].possibleDirections.push(Direction.Bottom);
+        this.maze[8].possibleDirections.push(Direction.Top);
+        //Coluna 2
+        this.maze[9].wallRight = true;
+        this.maze[9].wallBottom = true;
+        this.maze[9].possibleDirections.push(Direction.Left);
+        this.maze[9].possibleDirections.push(Direction.Top);
+        //Coluna 3
+        this.maze[10].wallRight = true;
+        this.maze[10].wallLeft = true;
+        this.maze[10].possibleDirections.push(Direction.Top);
+        this.maze[10].possibleDirections.push(Direction.Bottom);
+        //Coluna 4
+        this.maze[11].wallTop = true;
+        this.maze[11].wallRight = true;
+        this.maze[11].wallLeft = true;
+        this.maze[11].possibleDirections.push(Direction.Bottom);
+
+        //Linha 4
+        //Coluna 1
+        this.maze[12].wallBottom = true;
+        this.maze[12].possibleDirections.push(Direction.Right);
+        // this.maze[12].possibleDirections.push(Direction.Left);
+        this.maze[12].possibleDirections.push(Direction.Top);
+        //Coluna 2
+        this.maze[13].wallTop = true;
+        this.maze[13].wallBottom = true;
+        this.maze[13].possibleDirections.push(Direction.Left);
+        this.maze[13].possibleDirections.push(Direction.Right);
+        //Coluna 3
+        this.maze[14].wallBottom = true;
+        this.maze[14].possibleDirections.push(Direction.Top);
+        this.maze[14].possibleDirections.push(Direction.Left);
+        this.maze[14].possibleDirections.push(Direction.Right);
+        //Coluna 4
+        this.maze[15].wallBottom = true;
+        this.maze[15].wallRight = true;
+        this.maze[15].possibleDirections.push(Direction.Top);
+        this.maze[15].possibleDirections.push(Direction.Left);
+
+        return this.maze;
+    }
+}
